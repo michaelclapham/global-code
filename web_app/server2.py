884673a2f70f07d31c64dfcfe0a4285a7251047a
Michael Clapham <michaeljclapham@gmail.com>
2019-07-17 22:46:22 +0000

    Use port environment variable

--- web_app/server2.py
+++ web_app/server2.py
@@ -1,5 +1,6 @@
 from flask import *
 import json
+import os
 app = Flask(__name__)
 items = []
 @app.route('/')
@@ -24,4 +25,5 @@ def foo(name):
     return render_template('index.html', to=name)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=80)
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0',port=port)
