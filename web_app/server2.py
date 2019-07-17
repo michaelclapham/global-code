712091fb60be93542219628a21c0ee8952eb6340
Michael Clapham <michaeljclapham@gmail.com>
2019-07-17 16:04:02 +0000

    todo list :)

--- web_app/server2.py
+++ web_app/server2.py
@@ -1,19 +1,24 @@
 from flask import *
-
+import json
 app = Flask(__name__)
-
 items = []
-
 @app.route('/')
 def index():
-    return render_template('form.html', items=items)
+    return render_template('form2.html', items=items)
 
 @app.route('/add_todo')
 def add_todo():
-    item = request.args.get("item")
+    item = request.args.get('item')
+    print(item)
     items.append(item)
-    return redirect("http://localhost:5000/", code=302)
+    return redirect("/", code=302)
 
+@app.route('/get_todos')
+def get_todos():
+    resp = Response(json.dumps(items))
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
+ 
 @app.route('/foo/<name>')
 def foo(name):
     return render_template('index.html', to=name)
