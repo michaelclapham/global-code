6a82dee0386179de14b8346312bc40d9aeea733a
Michael Clapham <michaeljclapham@gmail.com>
2019-07-17 22:40:10 +0000

    Bind to 0.0.0.0:80

--- web_app/server2.py
+++ web_app/server2.py
@@ -24,4 +24,4 @@ def foo(name):
     return render_template('index.html', to=name)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost')
+    app.run(debug=True, host='0.0.0.0',port=80)
